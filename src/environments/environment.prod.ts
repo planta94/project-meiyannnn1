b5d6650971d43993dcf8e32294e8b05bc8eb6136
planta94 <chiamy94@gmail.com>
2026-07-29 20:49:17 +0800

    feat: initialize production and development environment configuration files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  googleMapsApiKey: 'AIzaSyAK0-vCQx9YaCI40D-HavYK2zAAeeKyXEI',
+  googleMapsApiKey: 'API_KEY',
   appUsername: 'meiyannnn1',
   appPassword: '88888888'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  googleMapsApiKey: 'AIzaSyAK0-vCQx9YaCI40D-HavYK2zAAeeKyXEI',
+  googleMapsApiKey: 'API_KEY',
   appUsername: 'meiyannnn1',
   appPassword: '88888888'
 };
